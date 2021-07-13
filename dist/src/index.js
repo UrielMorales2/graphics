06b827a10cbcecffc10985c4760eea7dacb66a15
UrielMorales <urielmorales@gmail.com>
2021-07-13 17:21:54 -0500

    final

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,7 +1,67 @@
-import { CanvasLocal } from './canvasLocal.js';
+import { Obj3D } from './Obj3D.js';
+import { CvHLines } from './CvHLines.js';
 let canvas;
 let graphics;
 canvas = document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
-const miCanvas = new CanvasLocal(graphics, canvas);
-miCanvas.paint();
+let cv;
+let obj;
+function leerArchivo(e) {
+    var archivo = e.target.files[0];
+    if (!archivo) {
+        return;
+    }
+    var lector = new FileReader();
+    lector.onload = function (e) {
+        var contenido = e.target.result;
+        mostrarContenido(contenido);
+        obj = new Obj3D();
+        if (obj.read(contenido)) {
+            //sDir = sDir1;
+            cv = new CvHLines(graphics, canvas);
+            cv.setObj(obj);
+            cv.paint();
+        }
+    };
+    lector.readAsText(archivo);
+}
+function mostrarContenido(contenido) {
+    var elemento = document.getElementById('contenido-archivo');
+    //
+    //readObject(new Input(contenido));
+    elemento.innerHTML = contenido;
+}
+function vp(dTheta, dPhi, fRho) {
+    if (obj != undefined) {
+        let obj = cv.getObj();
+        if (!obj.vp(cv, dTheta, dPhi, fRho))
+            alert('datos no validos');
+    }
+    else
+        alert('aun no has leido un archivo');
+}
+function eyeDownFunc() {
+    vp(0, 0.1, 1);
+}
+function eyeUpFunc() {
+    vp(0, -0.1, 1);
+}
+function eyeLeftFunc() {
+    vp(-0.1, 0, 1);
+}
+function eyeRightFunc() {
+    vp(0.1, 0, 1);
+}
+function incrDistFunc() {
+    vp(0, 0, 2);
+}
+function decrDistFunc() {
+    vp(0, 0, 0.5);
+}
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
+document.getElementById('eyeUp').addEventListener('click', eyeUpFunc, false);
+document.getElementById('eyeLeft').addEventListener('click', eyeLeftFunc, false);
+document.getElementById('eyeRight').addEventListener('click', eyeRightFunc, false);
+document.getElementById('incrDist').addEventListener('click', incrDistFunc, false);
+document.getElementById('decrDist').addEventListener('click', decrDistFunc, false);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { CanvasLocal } from './canvasLocal.js';
+
+import { Input } from './Input.js';
+import { Obj3D } from './Obj3D.js';
+import { Canvas3D } from './Canvas3D.js';
+import { CvWireframe } from './CvWireFrame.js';
+import { CvHLines } from './CvHLines.js';
 
 let canvas: HTMLCanvasElement;
 let graphics: CanvasRenderingContext2D;
@@ -6,6 +11,74 @@ let graphics: CanvasRenderingContext2D;
 canvas = <HTMLCanvasElement>document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
 
-const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas);
+let cv: CvHLines;
+let obj: Obj3D;
 
-miCanvas.paint();
+function leerArchivo(e) {
+  var archivo = e.target.files[0];
+  if (!archivo) {
+    return;
+  }
+  var lector = new FileReader();
+  lector.onload = function(e) {
+    var contenido = e.target.result;
+    mostrarContenido(contenido);
+    obj = new Obj3D();
+    if (obj.read(contenido)) {
+      //sDir = sDir1;
+      cv = new CvHLines(graphics, canvas);
+      cv.setObj(obj);
+      cv.paint();
+    }
+  };
+  lector.readAsText(archivo);
+}
+
+function mostrarContenido(contenido:any) {
+  var elemento = document.getElementById('contenido-archivo');
+  //
+  //readObject(new Input(contenido));
+  elemento.innerHTML = contenido;
+}
+
+function vp(dTheta:number, dPhi:number, fRho:number):void{  // Viewpoint
+  if (obj != undefined) {
+    let obj: Obj3D = cv.getObj();
+    if (!obj.vp(cv, dTheta, dPhi, fRho))
+      alert('datos no validos');
+  }
+  else
+    alert('aun no has leido un archivo');
+}
+
+function eyeDownFunc() {
+  vp(0, 0.1, 1);
+}
+
+function eyeUpFunc() {
+  vp(0, -0.1, 1);
+}
+
+function eyeLeftFunc() {
+  vp(-0.1, 0, 1);
+}
+
+function eyeRightFunc() {
+  vp(0.1, 0, 1);
+}
+
+function incrDistFunc() {
+  vp(0, 0, 2);
+}
+
+function decrDistFunc() {
+  vp(0, 0, 0.5);
+}
+
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
+document.getElementById('eyeUp').addEventListener('click', eyeUpFunc, false);
+document.getElementById('eyeLeft').addEventListener('click', eyeLeftFunc, false);
+document.getElementById('eyeRight').addEventListener('click', eyeRightFunc, false);
+document.getElementById('incrDist').addEventListener('click', incrDistFunc, false);
+document.getElementById('decrDist').addEventListener('click', decrDistFunc, false);
